422502f04706657d46013244a4708e1fc2af6fb2
Ak842931 <114336148+Ak842931@users.noreply.github.com>
2025-04-11 03:12:31 +0530

    Fixed DB config to connect to Clever Cloud

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -1,12 +1,13 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+define('DB_SERVER','bn8e5523ah4x7rwqatic-mysql.services.clever-cloud.com');
+define('DB_USER','ujnkiwi7gdjhfwtd');
+define('DB_PASS' ,'XjdZPyV0c6alWWEe4MA6');
+define('DB_NAME', 'bn8e5523ah4x7rwqatic');
+
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
 // Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
